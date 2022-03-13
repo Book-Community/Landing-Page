d1cdc173038394a3e1c35455d7b051dba65c9f90
ioannis Apostolou <giannistolou@protonmail.com>
2022-03-13 22:14:51 +0200

    blog mvp

--- compile/run.sh
+++ compile/run.sh
@@ -1,1 +1,4 @@
+sass ../style/mixins.scss ../style/mixins.css
 sass ../style/index.scss ../style/index.css
+sass ../style/blog.scss ../style/blog.css
+sass ../style/commonStyle.scss ../style/commonStyle.css

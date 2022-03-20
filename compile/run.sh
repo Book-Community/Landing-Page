8965155f110c63048dc0b601788b66b58da6b1f9
ioannis Apostolou <giannistolou@protonmail.com>
2022-03-20 15:27:49 +0200

    Book as image in hero

--- compile/run.sh
+++ compile/run.sh
@@ -1,1 +1,4 @@
+sass ../style/variables.scss ../style/variables.css
+sass ../style/mixins.scss ../style/mixins.css
+sass ../style/common.scss ../style/common.css
 sass ../style/index.scss ../style/index.css
